e0bc27d00b08b515c616d4faa9f8cf04082127c0
Alvan Tan <alvan.tan.2019@sis.smu.edu.sg>
2022-06-01 16:42:36 +0800

    completed

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import Create from './Create';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const google_site = "http://www.google.com";
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
